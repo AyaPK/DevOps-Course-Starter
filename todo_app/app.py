ddfc12ee26137329f5b03f9d6454efe5e45f948e
Aya Stead <aya.stead@softwire.com>
2024-01-16 13:29:12 +0000

    Allow adding and viewing of items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +8,9 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+@app.route('/item', methods=['POST'])
+def add_new_item():
+    add_item(request.form.get('name'))
+    return render_template('index.html', items=get_items())

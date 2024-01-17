70885647797e7da6caf714338007575d8daca632
Aya Stead <aya.stead@softwire.com>
2024-01-17 12:05:38 +0000

    Display cards on Index

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
+from todo_app.data.trello_items import get_all_lists, get_all_cards
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,7 +9,7 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=get_items())
+    return render_template('index.html', items=get_all_cards())
 
 @app.route('/item', methods=['POST'])
 def add_new_item():
